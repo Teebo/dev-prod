69bf1955406b9a6546789d3216e440538799983e
Thabo <thabo@basalt.co>
2020-02-10 13:25:35 +0200

    Fix script log file location

---
+++ scripts/logDevWork.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+let commitMessage = fs.readFileSync(process.argv[2], { encoding: "utf-8" });
+
+const body = {
+  devEmail: "emailll",
+  projectName: "projecname..",
+  logMessage: commitMessage,
+  logTime: "logtimmeee"
+};
+
+fetch("https://tranquil-crag-92279.herokuapp.com/api/devWorkHistoryLog", {
+  method: "post",
+  body: JSON.stringify(),
+  headers: { "Content-Type": "application/json" }
+})
+  .then(res => res.json())
+  .then(json => {
+    console.log(json, "dfd");
+  })
+  .catch(err => {
+    console.log("Error:", err);
+  });
