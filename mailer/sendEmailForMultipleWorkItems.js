8ad73d353ebb98187b900cbfcdaf5573fe149885
Thabo-ops <thabo@basalt.co>
2020-03-02 11:33:16 +0200

    Aim

--- mailer/sendEmailForMultipleWorkItems.js
+++ mailer/sendEmailForMultipleWorkItems.js
@@ -14,9 +14,13 @@ const generateEmailTemplate = (workItems, forHTML=false) => {
         <td style="border: 1px solid #dddddd;
         text-align: left;
         padding: 15px;">${workItem.title}</td>
-        <td style="border: 1px solid #dddddd;
-        text-align: left;
-        padding: 15px;">
+        <td style="color: #fff!important;
+        background-color: #2196F3!important;
+        width: auto;
+        height: auto;
+        padding: 8px;
+        text-decoration: none;
+        cursor: pointer">
           <a href="www.somewhere.com" class="action-button">Mark as in progress</a>
         </td>
       </tr>  
