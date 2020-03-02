c550682121464927b67ba355c8ba4cd6f39d2c70
Thabo-ops <thabo@basalt.co>
2020-03-02 12:08:29 +0200

    What happened

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connection.on('err', function(err) {
   console.log('Could not connect to mongoDB at', dbURI);
   console.log('******************************');
   console.log(err);
-  console.log('**************************8*****');
+  console.log('*******************************');
   process.exit(1);
 });
 
